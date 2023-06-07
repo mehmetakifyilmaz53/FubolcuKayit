9fe7d6f395386645fa846613ffc70a14556f7d27
mehmetakifyilmaz53 <mehmetakifyilmaz17054@gmail.com>
2023-06-07 19:16:11 +0300

    First Commit

---
+++ Futbolcu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_örnek
+{
+    public class Futbolcu
+    {
+        public string İsim;
+        public string Pozisyon;
+        public string Takımı;
+        public int Yaşı;
+        public int Maaşı;
+        public int SözleşmeYılı;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace class_örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Futbolcu futbolcu1 = new Futbolcu();
+            Console.WriteLine("isim Giriniz");
+            futbolcu1.İsim = Console.ReadLine();
+            //futbolcu1.İsim = "Mehmet Akif Yılmaz";
+            Console.WriteLine("Pozisyon");
+            futbolcu1.Pozisyon = Console.ReadLine();
+            //futbolcu1.Pozisyon = "Çakılı Stoper";
+            Console.WriteLine("Takımı giriniz");
+            futbolcu1.Takımı = Console.ReadLine();
+            //futbolcu1.Takımı = "Arsenal";
+            Console.WriteLine("Yaş Giriniz");
+            futbolcu1.Yaşı = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sözleşme yılı giriniz");
+            futbolcu1.SözleşmeYılı = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Futbolcu İsmi: " + futbolcu1.İsim);
+            Console.WriteLine("Futbolcu Pozisyonu: " + futbolcu1.Pozisyon);
+            Console.WriteLine("Futbolcu Takımı: " + futbolcu1.Takımı);
+            Console.WriteLine("Futbolcu Yaşı: " + futbolcu1.Yaşı);
+            Console.WriteLine("Futbolcu SözleşmeYılı: " + futbolcu1.SözleşmeYılı);
+
+
+
+        }
+    }
+}
